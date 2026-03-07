dcc07a383627e6f18a1fc1dd542c2864651ad73f
Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
2026-03-07 00:48:50 +0000

    Add defines for keycodes

--- custom_keys.h
+++ custom_keys.h
@@ -16,6 +16,30 @@
 
 #include QMK_KEYBOARD_H
 
+// Aliases for modified keys and UK symbols (backslash and pipe).
+
+#define A_ST S(KC_TAB)
+#define A_BS KC_NUBS
+#define A_PP S(KC_NUBS)
+
+// Aliases for cut, copy and paste.
+
+#define A_CX RCTL(KC_X)
+#define A_CC RCTL(KC_C)
+#define A_CV RCTL(KC_V)
+
+// 1Password shortcut for quick access.
+
+#define P_QA C(S(KC_SPC))
+#define P_AT S(KC_ENT)
+
+// Aliases for Windows previous and next tab / window / desktop.
+
+#define W_PT C(S(KC_TAB))
+#define W_NT C(KC_TAB)
+#define W_PD C(G(KC_LEFT))
+#define W_ND C(G(KC_RIGHT))
+
 // These functions need to be called from the equivalent _user() functions in
 // the keymap because the QMK module API does not support auto shift or caps
 // word. This can be accomplished by adding "SRC += custom_keys_user.c" to the
